9245e253e67390b4a8d3db1447ad8a3e7199bc98
Taejas <taejas16204@iiitd.ac.in>
2018-11-30 21:55:17 +0530

    Naive heuristics
    Single character subscript, superscript, =

---
+++ heuristics.py
@@ -0,0 +1,48 @@
+class segment(object):
+
+	def __init__(self, ch, x, y, w, h):
+		# Character
+		self.ch = ch
+		# x coordinate of centroid of bounding box
+		self.x = x
+		# y coordinate of centroid of bounding box
+		self.y = y
+		# width of bounding box
+		self.w = w
+		# height of bounding box
+		self.h = h
+
+
+
+# Function that returns the LaTeX code corresponding to the 
+def getLatex(segments):
+
+	expr = []
+	# Assuming that the first segment corresponds to a character that is... 'normal' script.
+	expr.append(segments[0].ch)
+
+	for i in range(len(segments)):
+		curr = segments[i]
+		prev = segments[i - 1]
+
+		# = test
+		if curr.ch == '-' and prev.ch == '-':
+			if curr.x - curr.w / 2 < prev.x + prev.w / 2:
+				expr[len(expr) - 1] = '='
+
+		# Subscript test
+		elif curr.y < prev.y - prev.h / 2:
+			expr.append('_{')
+			expr.append(curr.ch)
+			expr.append('}')
+
+		# Suberscript test
+		elif curr.y > prev.y + prev.h / 2:
+			expr.append('^{')
+			expr.append(curr.ch)
+			expr.append('}')
+
+		else:
+			expr.append(curr.ch)
+
+	return ''.join(expr)
