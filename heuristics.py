f58b2a9b6227182e565232f258c9d6005da1b92f
Taejas <taejas16204@iiitd.ac.in>
2018-12-02 22:14:29 +0530

    Heuristics
    sqrt
    equal to
    # division
    fraction
    minus
    subscript
    superscript

--- heuristics.py
+++ heuristics.py
@@ -3,9 +3,9 @@ class segment(object):
 	def __init__(self, ch, x, y, w, h):
 		# Character
 		self.ch = ch
-		# x coordinate of centroid of bounding box
+		# x coordinate of centroid of bounding box; increases from left to right
 		self.x = x
-		# y coordinate of centroid of bounding box
+		# y coordinate of centroid of bounding box; increases from top to bottom
 		self.y = y
 		# Width of bounding box
 		self.w = w
@@ -14,35 +14,105 @@ class segment(object):
 
 
 
+def latex_sqrt(seg, i, expr):
+	expr.append('\\sqrt')
+	root = seg[i]
+	j = i + 1
+	while j < len(seg):
+		if seg[j].x < root.x + root.w / 2:
+			expr.append(seg[j].ch)
+			i += 1
+	expr.append('}')
+	return i
+
+
+def latex_horizontal(seg, i, expr):
+
+	# Equal to
+	if i + 1 < len(seg) and seg[i + 1].ch == '-':
+		if seg[i + 1].x - seg[i + 1].w / 2 < seg[i].x + seg[i].w / 2:
+			expr.append('=')
+			return i + 2
+
+	# # Division
+	# if i + 2 < len(seg) and seg[i + 1].ch == '.' and seg[i + 2].ch == '.':
+	# 	if seg[i + 1].x < seg[i].x + seg[i].w / 2 and seg[i + 2].x < seg[i].x + seg[i].w / 2:
+	# 		if (seg[i + 1].y > seg[i].y and seg[i + 2].y < seg[i].y) or (seg[i + 1].y < seg[i].y and seg[i + 2].y > seg[i].y)
+	# 			expr.append(' \div ')
+	# 			return i + 3
+
+	# Fraction
+	if i + 2 < len(seg):
+		bar = seg[i]
+		num = []
+		den = []
+		j = i + 1
+		while j < len(seg) and seg[j].x < bar.x:
+			if(seg[j].y < bar[j].y):
+				num.append(seg[j].ch)
+			else:
+				den.append(seg[j].ch)
+		if num != [] and den != []:
+			expr.append('\\frac{' + ''.join(num) + '}{' + ''.join(den) + '}')
+			return j
+
+	# Minus
+	expr.append('-')
+	return i + 1
+
+
+def latex_subscript(seg, i, expr):
+	if seg[i + 1].y > seg[i].y + seg[i].h / 2:
+		base = seg[i]
+		j = i + 1
+		expr.append('_{')
+		while j < len(seg) and seg[j].y > base.y + base.h / 2:
+			expr.append(seg[j].ch)
+		expr.append('}')
+		return j
+	return i
+
+
+def latex_superscript(seg, i, expr):
+	if seg[i + 1].y + seg[i + 1].h / 2 < seg[i].y:
+		base = seg[i]
+		j = i + 1
+		expr.append('^{')
+		while j < len(seg) and seg[j].y + seg[j].h / 2 < base.y:
+			expr.append(seg[j].ch)
+		expr.append('}')
+		return j
+	return i
+
+
+
 # Function that returns the LaTeX code corresponding to the segments.
-def getLatex(segments):
+def get_latex_expr(segments):
 
 	expr = []
-	# Assuming that the first segment corresponds to a character that is... 'normal' script.
-	expr.append(segments[0].ch)
+	i = 0
 
-	for i in range(1, len(segments)):
+	while i < len(segments):
+
 		curr = segments[i]
 		prev = segments[i - 1]
 
-		# = test
-		if curr.ch == '-' and prev.ch == '-':
-			if curr.x - curr.w / 2 < prev.x + prev.w / 2:
-				expr[len(expr) - 1] = '='
+		# -
+		if curr.ch == '-':
+			i = latex_horizontal(segments, i, expr)
 
-		# Subscript test
-		elif curr.y < prev.y - prev.h / 2:
-			expr.append('_{')
-			expr.append(curr.ch)
-			expr.append('}')
+		# sqrt
+		elif curr.ch == 'sqrt':
+			i = latex_sqrt(segments, i, expr)
 
-		# Superscript test
-		elif curr.y > prev.y + prev.h / 2:
-			expr.append('^{')
-			expr.append(curr.ch)
-			expr.append('}')
 
 		else:
 			expr.append(curr.ch)
+			i_prev = i
+			i = expr.latex_subscript(segments, i, expr)
+			if(i == i_prev):
+				i = expr.latex_superscript(segments, i, expr)
+			if(i == i_prev):
+				i += 1
 
 	return ''.join(expr)
